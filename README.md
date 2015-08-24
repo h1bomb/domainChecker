1ac36d069b3a6ad50cfd7f9a6148131aa1e8671f
ZhouQiqi <qiqi.zhou@gmail.com>
2015-08-24 08:49:25 +0800

    add store support

---
+++ README.md
@@ -0,0 +1,21 @@
+Domain checker
+---------------
+
+CLI for www.net.cn check domain status.
+
+useage:
+
+`
+node wgRun
+`
+
+It will check domain with number and letter.
+
+For fun!
+
+
+todo list
+============
+
+* store useful domain on disk
+* make CLI user experience friendly 
--- examples/simple.js
+++ examples/simple.js
@@ -3,9 +3,9 @@ var Ck = require('../index');
 var ck = new Ck();
 
 ck.check('zhouqiqisaxa.xyz', function(error, response, body) {
-	if (!error) {
-		console.log(body);
-	} else {
-		console(error);
-	}
+    if (!error) {
+        console.log(body);
+    } else {
+        console(error);
+    }
 })
